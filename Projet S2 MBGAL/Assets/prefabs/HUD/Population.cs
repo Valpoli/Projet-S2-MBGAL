a004d5638ac54af3efcb685fd2611e893aa927d1
Valentin poli <valentin.poli@epita.fr>
2020-04-23 11:50:31 +0200

    on est largos

---
+++ Projet S2 MBGAL/Assets/prefabs/HUD/Population.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Population : MonoBehaviour
+{
+    public Text nbPop;
+
+    public GameObject pop;
+    // Start is called before the first frame update
+    void Update()
+    {
+        Game Pop = pop.GetComponent<Game>();
+        nbPop.text = ": " + Convert.ToString(Pop.popAct) + "/" + Convert.ToString(Pop.logementTot);
+    }
+
+    
+}
