33ce8ddff215279f787293240d0a6a20da7b615c
Evan Pernette <evan.pernette@epita.fr>
2020-05-20 17:19:09 +0200

    MENU INGAME

---
+++ SauverMBGAL/Assets/Script/Menu/MenuInGame.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuInGame : MonoBehaviour
+{
+    public GameObject Menu;
+    private bool MenuActive = false;
+    
+    
+    void Update()
+    {
+        if (MenuActive)
+        {
+            Menu.SetActive(MenuActive);
+            Time.timeScale = 0;
+        }
+        else
+        {
+            Menu.SetActive(MenuActive);
+            Time.timeScale = 1;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            MenuActive = true;
+        }
+        
+        
+    }
+
+    public void resume()
+    {
+        MenuActive = false;
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
